295108be63e0b02876f7e63006dd77ad69601c12
Dimash Janibekov <99655528+DizzyZ7@users.noreply.github.com>
2026-03-05 23:54:56 +0300

    Create models.py

---
+++ database/models.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Auction(Base):
+
+    __tablename__ = "auctions"
+
+    id = Column(Integer, primary_key=True)
+
+    title = Column(String)
+
+    topic_id = Column(Integer)
+
+    start_price = Column(Float)
+
+    current_price = Column(Float)
+
+    step = Column(Float)
+
+    end_time = Column(DateTime)
+
+    leader_id = Column(Integer)
+
+    message_id = Column(Integer)
+
+    status = Column(String, default="active")
+
+
+class Bid(Base):
+
+    __tablename__ = "bids"
+
+    id = Column(Integer, primary_key=True)
+
+    auction_id = Column(Integer)
+
+    user_id = Column(Integer)
+
+    amount = Column(Float)
+
+    time = Column(DateTime)
